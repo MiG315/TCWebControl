db491302513fa3891e20e7ee28e23d851c4764cf
m0sk1t <m0sk1t@bk.ru>
2013-11-25 17:01:19 +0400

    noname

--- app/assets/javascripts/testcontrol.js
+++ app/assets/javascripts/testcontrol.js
@@ -65,6 +65,7 @@
 			$.each(json.data, function(key,obj) {
 				$('#stand').append('<option value="' +
 				obj.value + '">' + obj.label + '</option>');
+				
 				$('#releasecoverage').append('<option value="' +
 				obj.value + '">' + obj.label + '</option>');
 			});
@@ -188,7 +189,7 @@
 			case 'backup':			sURLget = sURL+'getMDSBackup?compname='; break;
 			case 'getMDSByName':	sURLget = sURL+'getMDSByName?compname='; break;
 		}
-		$.getJSON(sURLget.toLowerCase() + $('#comp :selected').val() + '&standname=' + $('#stand :selected').val() + ((receiveMode==='getMDSByName')?'&mdsname='+$('#nameMDS').val():'')), function(json) {
+		$.getJSON(sURLget.toLowerCase() + $('#comp :selected').val() + '&standname=' + $('#stand :selected').val() + ((receiveMode==='getMDSByName')?'&mdsname='+$('#nameMDS').val():''), function(json) {
 			loadedData = json.data;
 			$("#testsTree")
 				.jstree({
